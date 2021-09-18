716b8ca064bf284bfe8e2745005c6e062f2f0a3f
Glitch (glitch-hello-website) <none>
2021-09-17 20:29:19 -0700

    first commit

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ if (btn) {
   btn.onclick = function() {
     // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
-    setTimeout(
-      function() {location.href = 'https://google.com';}
-    ,500);
+    setTimeout(function() {
+      location.href = "https://google.com";
+    }, 500);
   };
 }
